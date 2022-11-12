6c14c99a155206644de7d8e763f4530c2a7f1a70
ludoguenet <ludovicguenet@gmx.com>
2022-11-12 16:34:57 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1
+      v-text="title"
+  ></h1>
+
+  <h2
+      v-text="subTitle"
+  >
+  </h2>
+
+  <button
+    v-on:click="editTitle('Hello World Edited')"
+  >
+    éditer title
+  </button>
+
+  <button
+      v-on:click="editSubTitle('80')"
+  >
+    éditer subTitle
+  </button>
+</template>
+
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  // const state = reactive({
+  //   title: 'Hello World' as string,
+  //   subTitle: 90 as string | number,
+  // });
+
+  const title = ref<string>('Hello World');
+  const subTitle = ref<string | number>(90);
+
+  const editTitle = (editTitle: string) => {
+    title.value = editTitle;
+  }
+
+  const editSubTitle = (editSubTitle: string | number) => {
+    subTitle.value = editSubTitle;
+  }
+</script>
